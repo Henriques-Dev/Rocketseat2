8973bb3c1fa3640b760688a9c363087c12d4b644
Henriques-Dev <lucaschenriques@icloud.com>
2024-09-10 19:20:41 -0300

    listar metas

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { select, input } = require('@inquirer/prompts') // Requisitando e prometendo
+const { select, input, checkbox } = require('@inquirer/prompts') // Requisitando e prometendo
 
 let meta = {
     value: 'Tomar 3L de água por dia',
@@ -19,6 +19,33 @@ const cadastrarMeta = async () => {
         value: meta,
         checked: false
     })
+}
+
+const listarMetas = async () => {
+    const respostas = await checkbox({
+        message: "Use as setas para mudar de meta, o espaço para marcar ou desmarcar e o enter para finalizar essa etapa.",
+        choices: [...metas],
+        instructions: false,
+    })
+
+    if(respostas.length == 0) {
+        console.log('Nenhuma meta foi selecionada.')
+        return
+    }
+
+    metas.forEach((m) => {
+        m.checked = false
+    })
+
+    respostas.forEach((resposta) => { // Pesquisar a meta na lista Metas
+        const meta = metas.find((m) => {
+            return m.value == resposta
+        })
+
+        meta.checked = true
+    })
+
+    console.log('Meta(s) marcadas como concluída(s)')
 }
 
 const start = async () => {
@@ -49,6 +76,7 @@ const start = async () => {
                 console.log(metas)
                 break
             case "listar":
+                await listarMetas()
                 console.log("vamos listar")
                 break
             case "sair":
